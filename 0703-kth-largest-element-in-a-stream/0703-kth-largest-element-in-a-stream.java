810be351834b2f2183458749c201097a63431db0
Tushar Verma <tusharchaudhari8919@gmail.com>
2026-07-19 18:27:33 +0530

    Time: 23 ms (65.52%), Space: 51.9 MB (87.7%) - LeetHub

---
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.java
@@ -0,0 +1,26 @@
+class KthLargest {
+    PriorityQueue<Integer> pq= new PriorityQueue<>();
+    int k;
+
+
+    public KthLargest(int k, int[] nums) {
+        this.k=k;
+        for(int n : nums){
+            pq.offer(n);
+            if(pq.size()>k){
+                pq.poll();
+            }
+        }
+
+        
+    }
+    
+    public int add(int val) {
+        pq.offer(val);
+        if(pq.size()> k){
+            pq.poll();
+        }
+        return pq.peek();
+        
+    }
+}
