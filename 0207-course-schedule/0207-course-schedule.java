7a6183adeacfbf2567e7774ca0f9c11e60ae4e8f
Tushar Verma <tusharchaudhari8919@gmail.com>
2026-07-25 00:39:15 +0530

    Time: 7 ms (56.36%), Space: 46.9 MB (88.7%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,55 @@
+class Solution {
+
+    private List<List<Integer>> adj = new ArrayList<>();
+    private Map<Integer, String> state = new HashMap<>();
+    private boolean cycleFound = false;
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+            state.put(i, "U");
+        }
+
+        for (int i = 0; i < prerequisites.length; i++) {
+            int[] req = prerequisites[i];
+            int fromNode = req[1];
+            int toNode = req[0];
+            adj.get(fromNode).add(toNode);
+        }
+
+        for (int i = 0; i < numCourses; i++) {
+            if (state.get(i).equals("U")) {
+                dfs(i);
+            }
+
+            if (cycleFound) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void dfs(int node) {
+
+        if (state.get(node).equals("U")) {
+
+            state.put(node, "V");
+
+            for (int nei : adj.get(node)) {
+
+                dfs(nei);
+
+                if (cycleFound)
+                    return;
+            }
+
+            state.put(node, "P");
+        }
+
+        else if (state.get(node).equals("V")) {
+            cycleFound = true;
+        }
+    }
+}
