342f9b0d80ef2596171b45bc4c05e28d32a4a69a
Tushar Verma <tusharchaudhari8919@gmail.com>
2026-07-26 21:47:29 +0530

    Time: 0 ms (100%), Space: 44.8 MB (92.5%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,37 +1,42 @@
-class Solution {
-    public boolean search(int[] nums, int target) {
+class Solution{
+    public boolean search(int[]nums, int target){
+        int low=0;
+        int high=nums.length-1;
 
-        int left = 0;
-        int right = nums.length - 1;
-
-        while (left <= right) {
-
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target)
+        while(low<=high){
+            int mid= low+(high-low)/2;
+            if(nums[mid] == target){
                 return true;
 
-            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
-                left++;
-                right--;
-                continue;
+        }
+
+        if(nums[low]== nums[mid] && nums[mid]==nums[high]){
+            low++;
+            high--;
+            continue;
+        }
+        if(nums[low] <= nums[mid]){
+            if(target>= nums[low] && target<nums[mid]){
+                 high = mid-1;
             }
-
-            if (nums[left] <= nums[mid]) {
-
-                if (target >= nums[left] && target < nums[mid])
-                    right = mid - 1;
-                else
-                    left = mid + 1;
-            } else {
-
-                if (target > nums[mid] && target <= nums[right])
-                    left = mid + 1;
-                else
-                    right = mid - 1;
+            else{
+                 low=mid+1;
             }
+        }
+        else{
+            if(target> nums[mid] && target<=nums[high]){
+                low= mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
         }
 
         return false;
-    }
+        }
+
+    
+    
+        
 }
