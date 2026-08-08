bc0a58572df2e96e42aed99c48f6d27cad31d0ce
Tushar Verma <tusharchaudhari8919@gmail.com>
2026-08-08 11:25:32 +0530

    Time: 2 ms (99.32%), Space: 44.1 MB (91.97%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,15 +2,17 @@ class Solution {
     public boolean isPalindrome(String s) {
         int left =0,right= s.length()-1;
         while(left<=right){
-            char leftChar = Character.toLowerCase(s.charAt(left));
-            char rightChar = Character.toLowerCase(s.charAt(right));
-            if(!Character.isLetterOrDigit(leftChar)){
+            char leftC = Character.toUpperCase(s.charAt(left));
+            char rightC= Character.toUpperCase(s.charAt(right));
+
+            if(!Character.isLetterOrDigit(leftC)){
                 left++;
             }
-            else if(!Character.isLetterOrDigit(rightChar)){
+            else if(!Character.isLetterOrDigit(rightC)){
                 right--;
             }
-            else if(leftChar != rightChar){
+
+            else if(leftC != rightC){
                 return false;
             }
             else{
@@ -19,6 +21,7 @@ class Solution {
             }
         }
         return true;
+
         
     }
 }
