f30ca57791cc594dd876794a1c6c6e7861828b8b
Tushar Verma <tusharchaudhari8919@gmail.com>
2026-08-16 09:28:14 +0530

    Time: 7 ms (29.05%), Space: 114.7 MB (64.86%) - LeetHub

--- 2029-stone-game-ix/2029-stone-game-ix.java
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -1,18 +1,23 @@
 class Solution {
     public boolean stoneGameIX(int[] stones) {
 
-        int [] count= new int[3];
-        int n= stones.length;
+        int one = 0, two =0,zero=0;
 
         for(int num : stones){
-            count[num%3]++;
+            if(num % 3==0){
+                zero++;
+            }
+            else if( num % 3 ==1){
+                one++;
+            }
+            else{
+                two++;
+            }
         }
-
-        if(count[0] % 2 == 0){
-            return count[1]> 0 && count[2] > 0;
+        if(zero%2==0){
+            return one>0 && two > 0;
         }
-        return Math.abs(count[1] - count[2])>2;
-
+        return Math.abs(one - two)>2;
 
         
     }
