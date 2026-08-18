b2cc13eca8287391ad9205ff166a36e16aad2d40
Tushar Verma <tusharchaudhari8919@gmail.com>
2026-08-18 13:42:39 +0530

    Time: 1 ms (99.96%), Space: 46 MB (97.85%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -2,16 +2,23 @@ class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
 
         int[] count = new int[26];
+        int[] count1 = new int[26];
         for(char c : magazine.toCharArray()){
             count[c - 'a']++;
         }
         for(char c : ransomNote.toCharArray()){
-            count[c - 'a']--;
-            if(count[c-'a'] < 0){
+            count1[c - 'a']++;
+        }
+
+        for(int i =0; i<26;i++){
+            if(count1[i] > count[i]){
                 return false;
             }
         }
         return true;
+
+
+ 
 
 
 
