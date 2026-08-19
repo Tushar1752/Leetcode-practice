cc61064d195ab2230889b6a47adbf1725d9141cf
Tushar Verma <tusharchaudhari8919@gmail.com>
2026-08-19 11:04:40 +0530

    Time: 3 ms (42.44%), Space: 42.9 MB (85.44%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,24 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int[] countS = new int[26];
+        int[] countT = new int[26];
+
+        for(int i =0; i < s.length(); i++){
+            countS[s.charAt(i) - 'a']++;
+        }
+        for(int i =0 ; i<t.length() ; i++){
+            countT[t.charAt(i) - 'a']++;
+        }
+        for(int i=0; i<26;i++){
+
+            if(countS[i] != countT[i]){
+            return (char)(i + 'a');
+
+        }
+        }
+return ' ';
+
+
+        
+    }
+}
