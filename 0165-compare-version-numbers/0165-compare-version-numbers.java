436be439cacdf0d8f7f953ac75c670d80d9d5525
Tushar Verma <tusharchaudhari8919@gmail.com>
2026-08-23 14:45:02 +0530

    Time: 1 ms (75.44%), Space: 42.8 MB (60.68%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -4,15 +4,17 @@ class Solution {
         String[] a = v1.split("\\.");
         String[] b = v2.split("\\.");
 
-        int n = Math.max(a.length, b.length);
+        int i = 0;
 
-        for (int i = 0; i < n; i++) {
+        while (i < a.length || i < b.length) {
 
             int x = i < a.length ? Integer.parseInt(a[i]) : 0;
             int y = i < b.length ? Integer.parseInt(b[i]) : 0;
 
-            if (x < y) return -1;
-            if (x > y) return 1;
+            if (x != y)
+                return x < y ? -1 : 1;
+
+            i++;
         }
 
         return 0;
