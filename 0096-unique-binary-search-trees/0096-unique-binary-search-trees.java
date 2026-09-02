c958329a8ecf10ce575cc37cefd51d32bb601132
Tushar Verma <tusharchaudhari8919@gmail.com>
2026-09-02 19:45:34 +0530

    Time: 0 ms (100%), Space: 41.8 MB (84.29%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numTrees(int n) {
+        
+        int[] dp = new int[n + 1];
+
+        // Base cases
+        dp[0] = 1;
+        dp[1] = 1;
+
+        // Calculate number of BSTs for each number of nodes
+        for (int nodes = 2; nodes <= n; nodes++) {
+            
+            for (int root = 1; root <= nodes; root++) {
+                
+                int left = root - 1;
+                int right = nodes - root;
+                
+                dp[nodes] += dp[left] * dp[right];
+            }
+        }
+
+        return dp[n];
+    }
+}
