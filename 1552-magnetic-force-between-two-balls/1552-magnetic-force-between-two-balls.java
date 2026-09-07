395e742afeb76113fcfdfbd074da914012b4db2c
Tushar Verma <tusharchaudhari8919@gmail.com>
2026-09-07 19:10:06 +0530

    Time: 46 ms (70.94%), Space: 79.1 MB (45.74%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.java
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.java
@@ -3,33 +3,36 @@ import java.util.Arrays;
 class Solution {
 
     public int maxDistance(int[] position, int m) {
-
         Arrays.sort(position);
-
-        int low = 1;
-        int high = position[position.length - 1] - position[0];
-
-        while (low <= high) {
-
-            int mid = low + (high - low) / 2;
-
-            int count = 1;
-            int last = position[0];
-
-            // Check if we can place m balls
-            for (int i = 1; i < position.length; i++) {
-                if (position[i] - last >= mid) {
-                    count++;
-                    last = position[i];
-                }
+        int left=1;
+        int right=position[position.length -1]-position[0];
+        int answer=0;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(canPlace(position,m,mid)){
+                answer=mid;
+                left=mid+1;
             }
-
-            if (count >= m)
-                low = mid + 1;
-            else
-                high = mid - 1;
+            else{
+                right=mid-1;
+            }
         }
+        return answer;
 
-        return high;
+
+}
+public boolean canPlace(int[] position, int m,int distance){
+    int count =1;
+    int lastPosition=position[0];
+    for(int i=1;i<position.length;i++){
+        if(position[i] - lastPosition>=distance){
+            count++;
+            lastPosition = position[i];
+            if(count==m){
+                return true;
+            }
+        }
+    }
+        return false;
     }
 }
