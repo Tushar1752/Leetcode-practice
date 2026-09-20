a93fa607fb26dcc7c24c8b59662ca6a3436f5ec3
Tushar Verma <tusharchaudhari8919@gmail.com>
2026-09-20 12:44:35 +0530

    Time: 0 ms (100%), Space: 47.1 MB (46.03%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -4,9 +4,9 @@ class Solution {
         if(root==null){
             return 0;
         }
-        return Math.max(maxDepth(root.left) +1, maxDepth(root.right)+1);
 
-        
+        return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+
     }
 }
 
