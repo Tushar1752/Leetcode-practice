6c08732ec2deaec680e9c1f9f86d5327c8e6e03e
Tushar Verma <tusharchaudhari8919@gmail.com>
2026-09-25 16:35:48 +0530

    Time: 0 ms (100%), Space: 42.7 MB (18.44%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -3,10 +3,10 @@ class Solution {
         if(n<=0){
             return false;
         }
-        while(n%2==0){
-            n=n/2;
+        if((n & (n-1))==0){
+            return true;
         }
-return n==1;
+return false;
 
     }
 }
