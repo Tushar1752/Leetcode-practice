59a88780294e28237a3628f1904843aef0cef9c4
Tushar Verma <tusharchaudhari8919@gmail.com>
2026-09-26 13:47:27 +0530

    Time: 2 ms (93.84%), Space: 45.5 MB (91.3%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,28 +1,28 @@
 class Solution {
-    List<List<Integer>> ans = new ArrayList<>();
-
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        solve(candidates,target,0,new ArrayList<>());
-        return ans;
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
 
-        
+        backtrack(0,target, candidates, current, result);
+        return result;
+
+
     }
-    public void solve(int[] arr, int target,int index, List<Integer>list){
-        if(target==0){
-            ans.add(new ArrayList<>(list));
-            return ;
-        }
-        if (target<0 || index==arr.length){
+
+    public void backtrack(int index, int remaining,int[] candidates,List<Integer> current, List<List<Integer>> result ){
+        if(remaining==0){
+            result.add(new ArrayList<>(current));
             return;
         }
-        //Take current element
-        list.add(arr[index]);
-        solve(arr,target-arr[index],index,list);
-        //baccktrack
+        if(remaining <0){
+            return;
+        }
 
-        list.remove(list.size()-1);
-        //Dont take current element
+        for(int i = index; i<candidates.length; i++){
+            current.add(candidates[i]);
+            backtrack(i, remaining-candidates[i], candidates, current, result);
+            current.remove(current.size()-1);
+        }
 
-        solve(arr,target,index+1, list);
     }
 }
